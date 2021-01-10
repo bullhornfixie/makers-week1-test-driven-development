75dd6e2c35c23b7e20625785f22fa815e23db9a4
bullhornfixie <dawson.jon@hotmail.com>
2021-01-10 10:32:47 +0000

    refactor 1

--- dice_app/spec/dice_spec.rb
+++ dice_app/spec/dice_spec.rb
@@ -7,12 +7,12 @@ describe Dice do
   end 
   
   it "rolls dice with a range of 1-6" do
-    expect(subject.roll()).to be <=7
+    expect(subject.roll()).to be < 7
   end
   
   it "rolls 2 or more dice when passed an argument" do 
-    expect(subject.roll(2)).to be <=13
-    expect(subject.roll(3)).to be <=19
+    expect(subject.roll(2)).to be < 13
+    expect(subject.roll(3)).to be < 19
   end
   
   it "shows what your scores are for each roll" do
